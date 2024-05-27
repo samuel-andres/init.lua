ee476c1616e89ca9965ee8a19034e1f8e88337fb
Samuel Andrés <samuel5848@gmail.com>
2024-05-26 23:42:48 -0300

    FIX TABS

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -8,7 +8,7 @@ return {
                     enabled = true,
                     auto_trigger = true,
                     keymap = {
-                        accept = "<C-i>",
+                        accept = "<C-a>",
                     },
                 },
             }
