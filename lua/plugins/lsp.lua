a1538c2dc9cede0206115a1e77af114c6a81fe44
Samuel Andrés <samuel5848@gmail.com>
2024-05-28 00:12:42 -0300

    FORMATNT

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -241,13 +241,14 @@ return {
         },
         opts = {
             notify_on_error = false,
-            format_on_save = function(bufnr)
-                local disable_filetypes = { c = true, cpp = true }
-                return {
-                    timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                }
-            end,
+            -- uncomment the following to enable formatting on save
+            -- format_on_save = function(bufnr)
+            --     local disable_filetypes = { c = true, cpp = true }
+            --     return {
+            --         timeout_ms = 500,
+            --         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+            --     }
+            -- end,
             formatters_by_ft = {
                 lua = { "stylua" },
                 python = { "isort", "black" },
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,14 +3,12 @@ return {
         "echasnovski/mini.nvim",
         config = function()
             -- Better Around/Inside textobjects
-            --
             --  - va)  - [V]isually select [A]round [)]paren
             --  - yinq - [Y]ank [I]nside [N]ext [']quote
             --  - ci'  - [C]hange [I]nside [']quote
             require("mini.ai").setup { n_lines = 500 }
 
             -- Add/delete/replace surroundings (brackets, quotes, etc.)
-            --
             -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
             -- - sd'   - [S]urround [D]elete [']quotes
             -- - sr)'  - [S]urround [R]eplace [)] [']
@@ -18,7 +16,6 @@ return {
 
             -- Simple and easy statusline.
             local statusline = require "mini.statusline"
-            -- set use_icons to true if you have a Nerd Font
             statusline.setup { use_icons = vim.g.have_nerd_font }
 
             ---@diagnostic disable-next-line: duplicate-set-field
