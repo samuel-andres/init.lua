5c83d245a552c267c574e589a56d4b2048bfc930
Samuel Andrés <samuel5848@gmail.com>
2024-05-30 23:52:48 -0300

    neck pain toggleable

--- lua/plugins/center.lua
+++ lua/plugins/center.lua
@@ -1,8 +1,13 @@
 return {
     "shortcuts/no-neck-pain.nvim",
-    opts = {
-        autocmds = {
-            enableOnVimEnter = true,
-        },
-    },
+    config = function()
+        require("no-neck-pain").setup {
+            vim.keymap.set(
+                "n",
+                "<leader>cc",
+                "<CMD>NoNeckPain<CR>",
+                { desc = "Toggle center buffer (horizontally)" }
+            ),
+        }
+    end,
 }
