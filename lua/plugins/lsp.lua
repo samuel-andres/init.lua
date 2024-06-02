2b2e81fff7e7a873584ccc775259ed34a2d2b678
Samuel Andrés <samuel5848@gmail.com>
2024-06-02 18:28:43 -0300

    treesitter context

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -168,7 +168,6 @@ return {
                                 pyflakes = { enabled = false },
                                 pycodestyle = { enabled = false },
                             },
-                            configurationSources = { "flake8" },
                         },
                     },
                 },
@@ -189,6 +188,7 @@ return {
                 "html-lsp",
                 "css-lsp",
                 "tsserver",
+                "clangd",
             })
             require("mason-tool-installer").setup {
                 ensure_installed = ensure_installed,
@@ -317,6 +317,26 @@ return {
                     { name = "luasnip" },
                     { name = "path" },
                 },
+            }
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        opts = {},
+        config = function()
+            require("treesitter-context").setup {
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 1, -- Maximum number of lines to show for a single context
+                trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+                -- Separator between context and content. Should be a single character string, like '-'.
+                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                separator = nil,
+                zindex = 20, -- The Z-index of the context window
+                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
             }
         end,
     },
