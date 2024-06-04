6d2551c2a67073e3dd687097a69f7225c3779a77
Samuel Andrés <samuel5848@gmail.com>
2024-06-03 21:42:14 -0300

    mini

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,4 @@
 return {
-    -- "gc" to comment visual regions/lines
-    { "numToStr/Comment.nvim", opts = {} },
-    -- Highlight todo, notes, etc in comments
     {
         "folke/todo-comments.nvim",
         event = "VimEnter",
