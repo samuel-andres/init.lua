3854adfcc448674791fef21ff1514a402d2e6428
Samuel Andrés <samuel5848@gmail.com>
2024-08-26 23:39:07 -0300

    fix: remove deprecated opt for conform.nvim

--- lua/plugins/format.lua
+++ lua/plugins/format.lua
@@ -5,7 +5,7 @@ return {
   keys = {
     {
       '<leader>f',
-      function() require('conform').format { async = true, lsp_fallback = true } end,
+      function() require('conform').format { async = true, lsp_format = 'fallback' } end,
       mode = '',
       desc = '[F]ormat buffer',
     },
@@ -14,7 +14,13 @@ return {
     notify_on_error = false,
     format_on_save = function(bufnr)
       local disable_filetypes = { c = true, cpp = true }
-      return { timeout_ms = 500, lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype] }
+      local lsp_format_opt
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        lsp_format_opt = 'never'
+      else
+        lsp_format_opt = 'fallback'
+      end
+      return { timeout_ms = 500, lsp_format = lsp_format_opt }
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
