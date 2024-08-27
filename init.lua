66599aaabad1de50d539cfb63cea36cdac4c52ce
Samuel Andrés <samuel5848@gmail.com>
2024-08-27 16:46:34 -0300

    refactor

--- init.lua
+++ init.lua
@@ -1,17 +1,55 @@
-require 'options'
-require 'keymaps'
+-- [[ Options ]]
+vim.g.mapleader = ' '
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = 'yes'
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
+vim.opt.scrolloff = 10
+vim.opt.wrap = false
+vim.opt.colorcolumn = '80'
+vim.opt.termguicolors = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.o.foldcolumn = '0'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+vim.opt.termguicolors = true
+vim.opt.guicursor = ''
+vim.schedule(function() vim.opt.clipboard = 'unnamedplus' end)
 
+-- [[ Keymaps ]]
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>')
+vim.keymap.set('n', '[d', function() vim.diagnostic.jump { count = -1, float = true } end, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', 'd]', function() vim.diagnostic.jump { count = 1, float = true } end, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<C-w><S-j>', ':resize -5<CR>', { desc = 'Resize window down', silent = true })
+vim.keymap.set('n', '<C-w><S-k>', ':resize +5<CR>', { desc = 'Resize window up', silent = true })
+vim.keymap.set('n', '<C-w><S-l>', ':vertical resize -5<CR>', { desc = 'Resize window right', silent = true })
+vim.keymap.set('n', '<C-w><S-h>', ':vertical resize +5<CR>', { desc = 'Resize window left', silent = true })
+
+-- [[ Plugins ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-
 ---@diagnostic disable-next-line: undefined-field
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
 end
-
 vim.opt.rtp:prepend(lazypath)
-
 require('lazy').setup {
   spec = 'plugins',
   performance = {
