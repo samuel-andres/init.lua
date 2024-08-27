6f13ab8240639abc6a489863fbfc58389e6416c9
Samuel Andrés <samuel5848@gmail.com>
2024-08-26 23:45:20 -0300

    improve lazy load conform

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,6 +1,7 @@
 return {
   'hrsh7th/nvim-cmp',
-  event = 'InsertEnter',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
   dependencies = {
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-nvim-lsp',
