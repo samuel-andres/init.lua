83ffe378fe12fa8788d9fa62b4747a9a3f520cac
Samuel Andrés <samuel5848@gmail.com>
2024-09-11 19:55:51 -0300

    remove buffer manager

--- lua/plugins/buffers.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'j-morano/buffer_manager.nvim',
-  event = 'VimEnter',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    require('buffer_manager').setup()
-    vim.keymap.set('n', '<C-e>', function() require('buffer_manager.ui').toggle_quick_menu() end)
-  end,
-}
